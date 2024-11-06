4005a537726ac9bb58f011111e7729e3ad4c7176
Bart <bartolomej.kozorog@gmail.com>
2024-11-06 16:32:22 +0100

    update test case to demonstrate graph updates

--- executor/parallel/executor_test.go
+++ executor/parallel/executor_test.go
@@ -18,8 +18,12 @@ func TestRealWorldIndependentTransactions(t *testing.T) {
 		Transactions: []types.Transaction{
 			// First 3 transactions are inter-dependant
 			transactions.Transfer{From: "A", To: "B", Value: 10},
-			transactions.Mint{To: "A", Value: 10},
-			transactions.Transfer{From: "A", To: "C", Value: 10},
+			transactions.Mint{To: "A", Value: 20},
+			transactions.Transfer{From: "A", To: "C", Value: 20},
+			// This will first be treated as independent,
+			// but will be correctly moved to the dependency subgraph
+			// after the above transaction is re-executed.
+			transactions.Transfer{From: "C", To: "D", Value: 20},
 			// Next 3 transactions are independent
 			transactions.Transfer{From: "E", To: "F", Value: 10},
 			transactions.Transfer{From: "G", To: "H", Value: 10},
@@ -47,16 +51,17 @@ func TestRealWorldIndependentTransactions(t *testing.T) {
 	// Tx 1 has no inputs, so no other transaction can affect it's output.
 	assertExecutionCountEqual(t, &txExecutor, 1, 1)
 	assertExecutionCountEqual(t, &txExecutor, 2, 2)
-	assertExecutionCountEqual(t, &txExecutor, 3, 1)
+	assertExecutionCountEqual(t, &txExecutor, 3, 2)
 	assertExecutionCountEqual(t, &txExecutor, 4, 1)
 	assertExecutionCountEqual(t, &txExecutor, 5, 1)
+	assertExecutionCountEqual(t, &txExecutor, 6, 1)
 
 	expectedDelta := accountDelta{
 		oldState: startState,
 		updatedBalances: map[string]int{
 			"A": 0,
 			"B": 10,
-			"C": 10,
+			"D": 20,
 			"E": 0,
 			"G": 0,
 			"I": 0,
